6aa6d82476f4c4e4219e130209bf4457f2c1a73c
cdolm92 <cdolm92@gmail.com>
2016-01-19 16:24:33 -0500

    Initial Commit

---
+++ whosDown/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  whosDown
+//
+//  Created by Christella on 1/19/16.
+//  Copyright © 2016 Christella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ whosDown/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  whosDown
+//
+//  Created by Christella on 1/19/16.
+//  Copyright © 2016 Christella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
